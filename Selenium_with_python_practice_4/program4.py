096daaff0987f90cd40dc169c02fa65630467a59
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-06 17:01:31 +0530

    Update program4.py

--- Selenium_with_python_practice_4/program4.py
+++ Selenium_with_python_practice_4/program4.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -7,60 +6,25 @@ driver = webdriver.Firefox()
 
 driver.get("https://demo.automationtesting.in/Alerts.html")
 
-driver.find_element(By.XPATH,"//button[@class='btn btn-danger']").click()
-
-time.sleep(5)
-
+# Simple alert
+driver.find_element(By.XPATH, "//button[@class='btn btn-danger']").click()
+time.sleep(2)
 print(driver.switch_to.alert.text)
-
 driver.switch_to.alert.accept()
 
-driver.find_element(By.XPATH,"//button[@class='btn btn-danger']").click()
-
-time.sleep(5)
-
+# Navigate to CancelTab (Confirm alert)
+driver.find_element(By.XPATH, "//a[@href='#CancelTab']").click()
+driver.find_element(By.XPATH, "//button[@class='btn btn-primary']").click()
+time.sleep(2)
 print(driver.switch_to.alert.text)
-
-driver.switch_to.alert.accept() 
-
-driver.find_element(By.XPATH,"//a[@href='#CancelTab']").click()
-
-driver.find_element(By.XPATH,"//button[@class='btn btn-primary']").click()
-
-time.sleep(5)
-
-print(driver.switch_to.alert.text)
-
 driver.switch_to.alert.dismiss()
 
-driver.find_element(By.XPATH,"//button[@class='btn btn-danger']").click()
-
-time.sleep(5)
-
+# Navigate to TextboxTab (Prompt alert)
+driver.find_element(By.XPATH, "//a[@href='#Textbox']").click()
+driver.find_element(By.XPATH, "//button[@class='btn btn-info']").click()
+time.sleep(2)
 print(driver.switch_to.alert.text)
-
-driver.switch_to.alert.accept() 
-
-
-driver.find_element(By.XPATH,"//a[@href='#CancelTab']").click()
-
-driver.find_element(By.XPATH,"//button[@class='btn btn-primary']").click()
-
-time.sleep(5)
-
-print(driver.switch_to.alert.text)
-
-driver.switch_to.alert.dismiss()
-'''
-
-driver.find_element(By.XPATH,"//a[@href='#Textbox']").click()
-
-driver.find_element(By.XPATH,"//button[@class='btn btn-info']").click()
-
-time.sleep(5)
-
-print(driver.switch_to.alert.text)
-
 driver.switch_to.alert.send_keys("TESTER")
-
 driver.switch_to.alert.accept()
+
+driver.quit()
