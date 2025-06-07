bf32f56d304aa89c21cc85ac4bef83b3f481a225
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-07 22:10:57 +0530

    Update program5.py

--- Selenium_eith_python_practice_5/program5.py
+++ Selenium_eith_python_practice_5/program5.py
@@ -1,1 +1,34 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+
+driver.get("https://demo.automationtesting.in/Frames.html")
+
+# used to switch to single frame
+driver.switch_to.frame("singleframe")
+
+driver.find_element(By.XPATH,"//input[@type='text']").send_keys("TESTA")
+
+driver.switch_to.default_content()
+
+driver.find_element(By.XPATH,"//a[@href='#Multiple']").click()
+
+
+driver.switch_to.frame("singleframe")
+
+driver.find_element(By.XPATH,"//input[@type='text']").send_keys("TESTA")
+
+time.sleep(5)
+
+driver.switch_to.default_content()
+
+driver.find_element(By.XPATH,"//a[@href='#Multiple']").click()
+
+
+driver.switch_to.frame(driver.find_element(By.XPATH,"//iframe[@src='MultipleFrames.html']"))
+driver.switch_to.frame(driver.find_element(By.XPATH,"//iframe[@src='SingleFrame.html']"))
+
+driver.find_element(By.XPATH,"//input[@type='text']").send_keys("TESTB")
