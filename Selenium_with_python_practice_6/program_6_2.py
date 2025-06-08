d1d786e2478e847c22da96cf43de88f1e00d8c5a
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-08 14:10:39 +0530

    Update program_6_2.py

--- Selenium_with_python_practice_6/program_6_2.py
+++ Selenium_with_python_practice_6/program_6_2.py
@@ -1,1 +1,16 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Firefox()
+
+driver.get("https://demo.guru99.com/test/drag_drop.html")
+
+ActionChains(driver).scroll_by_amount(0,400).perform()
+
+src = driver.find_element(By.XPATH,"//li[@id='credit2']")
+trg = driver.find_element(By.XPATH,"//ol[@id='bank']")
+
+ActionChains(driver).drag_and_drop(src,trg).perform()
