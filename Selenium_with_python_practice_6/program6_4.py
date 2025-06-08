694de7e0d44ff2f089048962f0692416d4f55480
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-08 14:12:52 +0530

    Update program6_4.py

--- Selenium_with_python_practice_6/program6_4.py
+++ Selenium_with_python_practice_6/program6_4.py
@@ -1,1 +1,15 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.myntra.com/")
+
+src = driver.find_element(By.XPATH,"//div[@class='desktop-navLinks']//child::div//a[text()='Genz']")
+
+ActionChains(driver).move_to_element(src).perform()
+
+time.sleep(5)
