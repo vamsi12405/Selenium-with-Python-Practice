4ef9a5ba619ddc6ff915a8b0e5b062a59662962a
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-08 14:14:34 +0530

    Update program7_2.py

--- Selenium_with_python_practice_7/program7_2.py
+++ Selenium_with_python_practice_7/program7_2.py
@@ -1,1 +1,16 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+
+driver.get("https://demo.guru99.com/test/simple_context_menu.html")
+
+src = driver.find_element(By.XPATH,"//button[text()='Double-Click Me To See Alert']")
+
+ActionChains(driver).double_click(src).perform()
+
+
+time.sleep(5)
