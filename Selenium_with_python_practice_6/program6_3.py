66578b735ab271c22de62d43579f53a72c7142a3
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-08 14:11:51 +0530

    Update program6_3.py

--- Selenium_with_python_practice_6/program6_3.py
+++ Selenium_with_python_practice_6/program6_3.py
@@ -1,1 +1,15 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+
+driver.get("https://demo.automationtesting.in/Register.html")
+
+src = driver.find_element(By.ID,"submitbtn")
+
+ActionChains(driver).scroll_to_element(src).perform()
+
+time.sleep(5)
