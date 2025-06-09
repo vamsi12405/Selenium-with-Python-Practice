060b4ebb3bc597034712397d6d613d2c4fe4758b
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-09 18:52:27 +0530

    Update execute.py

--- Selenium_with_python_11/execute.py
+++ Selenium_with_python_11/execute.py
@@ -1,1 +1,16 @@
+from SeleniumProject import Framework as fw
 
+def verifyHomePage(driver):
+    fw.verifyTitle(driver,"Swag Labs")
+    fw.verifyCurrentUrl(driver,"https://www.saucedemo.com/")
+    fw.getTextAndVerify(driver,"//div[@class='login_logo']","Swag Labs")
+
+def verifyUserDeatils(driver):
+    fw.verifyElementIsDisplay(driver,"//input[@data-test='username']")
+    fw.verifyElementIsDisplay(driver, "//input[@data-test='password']")
+    fw.verifyElementIsDisplay(driver, "//input[@data-test='login-button']")
+
+def logIn(driver):
+    fw.type(driver,"//input[@data-test='username']","standard_user")
+    fw.type(driver, "//input[@data-test='password']", "secret_sauce")
+    fw.clickIn(driver,"//input[@data-test='login-button']")
