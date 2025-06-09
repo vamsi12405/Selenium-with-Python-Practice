4b683de7de78a26f4b1bc2b1dea59f11f6b7ab54
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-09 18:44:10 +0530

    Update program9_3.py

--- Selenium_with_python_9/program9_3.py
+++ Selenium_with_python_9/program9_3.py
@@ -1,1 +1,14 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+driver.get("https://demo.automationtesting.in/Register.html")
+
+driver.find_element(By.XPATH,"//input[@value='Cricket']").click()
+
+assert driver.find_element(By.XPATH,"//input[@value='Cricket']").is_selected(),"Cricket Field is not selected in web page"
+
+time.sleep(4)
