0b5b2f1192fe3f8bf9aaada7080dd8c096ff6128
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-09 19:00:14 +0530

    Update program12.py

--- Selenium_with_python_12/program12.py
+++ Selenium_with_python_12/program12.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as exp
+from selenium.webdriver.support.wait import WebDriverWait
 
+driver = webdriver.Chrome()
+driver.implicitly_wait(25)
+driver.get("https://www.saucedemo.com/")
+driver.find_element(By.ID,"user-name").send_keys("TEST")
+
+wait = WebDriverWait(driver,20,poll_frequency=10)
+wait.until(exp.presence_of_element_located((By.ID,"password")))
+wait.until(exp.alert_is_present())
+wait.until(exp.title_is())
+wait.until(exp.new_window_is_opened())
+wait.until(exp.frame_to_be_available_and_switch_to_it(("")))
+
+driver.find_element(By.ID,"passwords").send_keys("TEST")
