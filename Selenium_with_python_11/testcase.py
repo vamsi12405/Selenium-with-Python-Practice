bd45232dcb2269927920b17909c09f287f486790
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-09 18:54:45 +0530

    Update testcase.py

--- Selenium_with_python_11/testcase.py
+++ Selenium_with_python_11/testcase.py
@@ -1,1 +1,13 @@
+from SeleniumProject import Framework as fw
 
+
+def verifyProductsPage(driver):
+    fw.verifyTitle(driver,"Swag Labs")
+    fw.verifyCurrentUrl(driver,"https://www.saucedemo.com/inventory.html")
+    fw.getTextAndVerify(driver,"//div[@class='app_logo']","Swag Labs")
+
+
+def addToCart(driver):
+    fw.clickIn(driver,"//button[@data-test='add-to-cart-sauce-labs-backpack']")
+    fw.clickIn(driver, "//button[@data-test='add-to-cart-sauce-labs-bike-light']")
+    fw.clickIn(driver, "//button[@data-test='add-to-cart-sauce-labs-bolt-t-shirt']")
