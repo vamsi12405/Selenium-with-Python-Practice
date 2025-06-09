64eccfec99131b38a7dd1acc6b2bdc93b6c01c70
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-09 18:43:34 +0530

    Update program9_2.py

--- Selenium_with_python_9/program9_2.py
+++ Selenium_with_python_9/program9_2.py
@@ -1,1 +1,13 @@
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+assert driver.find_element(By.ID,"user-name").is_enabled(),"Username Text Field is disabled in web page"
+
+driver.find_element(By.ID,"user-name").send_keys("test@gmail.com")
+time.sleep(4)
