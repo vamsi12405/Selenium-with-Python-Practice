215a84b46516a5f36be786b3abf81ca731eafc7f
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-09 18:55:30 +0530

    Update finalexec.py

--- Selenium_with_python_11/finalexec.py
+++ Selenium_with_python_11/finalexec.py
@@ -1,1 +1,19 @@
+import time
 
+from selenium import webdriver
+from SeleniumProject import HomePage as hp
+from SeleniumProject import Framework as fw
+from SeleniumProject import ProductsPage as pp
+
+driver = webdriver.Chrome()
+
+def testCase01():
+    fw.application(driver,"https://www.saucedemo.com/")
+    hp.verifyHomePage(driver)
+    hp.verifyUserDeatils(driver)
+    hp.logIn(driver)
+
+def testCase02():
+    pp.verifyProductsPage(driver)
+    pp.addToCart(driver)
+    time.sleep(10)
