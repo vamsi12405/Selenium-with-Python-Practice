382db936befa864e307d4a72016ee833f7cfcdd4
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-09 18:47:34 +0530

    Update program10.py

--- Selenium_with_python_10/program10.py
+++ Selenium_with_python_10/program10.py
@@ -1,1 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
+driver.get("https://demo.guru99.com/test/web-table-element.php")
+
+expectedHeaderList = ["Company","Group","Prev Close (Rs)","Current Price (Rs)","% Change"]
+
+listOfCompanyElements = driver.find_elements(By.XPATH,"//table[@class='dataTable']//tbody//tr//td")
+
+count=0
+for companyElement in listOfCompanyElements:
+    print(companyElement.text,end=",")
+    count+=1
+    if count==5:
+        print("")
+        count=0
